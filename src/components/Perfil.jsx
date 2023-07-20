d679b444e9d91f430a6c974090966dc95f76c446
lorenzch2 <lorenzochacano@gmail.com>
2023-07-20 19:06:05 -0400

    perfil & navbar

--- src/components/Perfil.jsx
+++ src/components/Perfil.jsx
@@ -1,52 +1,67 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import { Card} from "react-bootstrap";
-
+import { Card } from "react-bootstrap";
+import "../assets/css/Perfil.css";
 
 const Perfil = () => {
-    const [userData, setUserData] = useState(null);
+  const [userData, setUserData] = useState(null);
 
-    useEffect(() => {
-        // Reemplaza 'TU_JWT_TOKEN' por el token JWT válido que tienes
-        const getTokenFromLocalStorage = localStorage.getItem("token");
-        axios
-            .get("http://localhost:3000/perfil", {
-                headers: {
-                    Authorization: `Bearer ${getTokenFromLocalStorage}`,
-                },
-            })
-            .then((response) => {
-                setUserData(response.data);
-            })
-            .catch((error) => {
-                console.error("Error al obtener los datos del perfil:", error);
-            });
-    }, []);
+  useEffect(() => {
+    // Reemplaza 'TU_JWT_TOKEN' por el token JWT válido que tienes
+    const getTokenFromLocalStorage = localStorage.getItem("token");
+    axios
+      .get("http://localhost:3000/perfil", {
+        headers: {
+          Authorization: `Bearer ${getTokenFromLocalStorage}`,
+        },
+      })
+      .then((response) => {
+        setUserData(response.data);
+      })
+      .catch((error) => {
+        console.error("Error al obtener los datos del perfil:", error);
+      });
+  }, []);
 
-    return (
-        <div>
-            {userData ? (
-                <Card
-                    className="cardDetail d-flex flex-row"
-                    style={{
-                        backgroundColor: "#295b6fff",
-                        color: "#ebca6d",
-                        border: "1px solid white",
-                    }}
-                >
-                    <div className="card-body">
-                        <h5 className="card-title">Nombre: {userData.nombre}</h5>
-                        <h5 className="card-title">Apellido: {userData.apellido}</h5>
-                        <p className="card-text">E-mail: {userData.email}</p>
-                        <p className="card-text">Dirección: {userData.direccion}</p>
-                        <p className="card-text">Teléfono: {userData.telefono}</p>
-                    </div>
-                </Card>
-            ) : (
-                <p>Cargando...</p>
-            )}
-        </div>
-    );
+  return (
+    <div>
+      {userData ? (
+        <Card
+          className="cardDetail d-flex flex-row"
+          style={{
+            backgroundColor: "#295b6fff",
+            color: "#ebca6d",
+            border: "1px solid white",
+          }}
+        >
+          <div className="card-body table-perfil">
+            <table className="container">
+              <tbody>
+                <tr className="tr-table">
+                  <th>Nombre: </th>
+                  <th>{userData?.nombre} {userData?.apellido}</th>
+                </tr>
+                <tr className="tr-table">
+                  <td>E-mail: </td>
+                  <td>{userData.email}</td>
+                </tr>
+                <tr className="tr-table">
+                  <td>Direccion:</td>
+                  <td>{userData.direccion}</td>
+                </tr>
+                <tr className="tr-table">
+                  <td>Telefono</td>
+                  <td>{userData.telefono}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </Card>
+      ) : (
+        <p>Cargando...</p>
+      )}
+    </div>
+  );
 };
 
 export default Perfil;
