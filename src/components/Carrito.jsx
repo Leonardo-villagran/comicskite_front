a7f368d5e374f01ac1c71e198512ee5a62b4728a
lorenzch2 <lorenzochacano@gmail.com>
2023-07-25 18:34:50 -0400

    d-none md carrito

--- src/components/Carrito.jsx
+++ src/components/Carrito.jsx
@@ -5,18 +5,19 @@ import Context from "../Context/Context";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "../assets/css/Carrito.css";
-import {formatearNumeroConPunto} from '../services/servicesNumbers';
+import { formatearNumeroConPunto } from "../services/servicesNumbers";
 
 const base_url = import.meta.env.VITE_API_URL;
 
 const Carrito = () => {
-
   const { carrito, setCarrito } = useContext(Context);
   const [detalleFinal, setDetalleFinal] = useState("");
   const [isProcessing, setIsProcessing] = useState(false);
 
   const eliminarDelCarrito = (id_producto) => {
-    const nuevosProductos = carrito.filter((producto) => producto.id_producto !== id_producto);
+    const nuevosProductos = carrito.filter(
+      (producto) => producto.id_producto !== id_producto
+    );
     setCarrito(nuevosProductos);
     toast.error("Producto eliminado del carrito");
   };
@@ -46,9 +47,11 @@ const Carrito = () => {
   };
 
   const getTotalGeneral = () => {
-    return carrito.reduce((total, producto) => total + getTotalPorProducto(producto), 0);
+    return carrito.reduce(
+      (total, producto) => total + getTotalPorProducto(producto),
+      0
+    );
   };
-
 
   const procesarCarrito = () => {
     setIsProcessing(true); // Bloquear el botón al inicio de la solicitud
@@ -59,23 +62,26 @@ const Carrito = () => {
       cantidad: producto.cantidad,
     }));
 
-    axios.post(base_url + "/carrito",
-      { detalle_productos: productosEnCarrito },
-      {
-        headers: {
-          Authorization: `Bearer ${getTokenFromLocalStorage}`,
-        },
-      })
+    axios
+      .post(
+        base_url + "/carrito",
+        { detalle_productos: productosEnCarrito },
+        {
+          headers: {
+            Authorization: `Bearer ${getTokenFromLocalStorage}`,
+          },
+        }
+      )
       .then((response) => {
         // Si la solicitud es exitosa, limpiamos el carrito
         setCarrito([]);
         console.log(response.data);
         toast.success("Carrito procesado con éxito.");
         // Concatenamos los saltos de línea con "<br />" para mostrarlos correctamente en el navegador
-        const detalleFinalConSaltosDeLinea = response.data.detalle_final.replace(/\n/g, "<br />");
+        const detalleFinalConSaltosDeLinea =
+          response.data.detalle_final.replace(/\n/g, "<br />");
         setDetalleFinal(detalleFinalConSaltosDeLinea);
         setIsProcessing(false);
-
       })
       .catch((error) => {
         console.error("Error al procesar el carrito:", error);
@@ -85,80 +91,118 @@ const Carrito = () => {
   return (
     <div>
       {/* Mostrar "Cargando..." mientras los datos se están cargando */}
-      <ToastContainer position="top-right" autoClose={1000} newestOnTop /><>
-          {carrito.length===0 && !detalleFinal ? <div><p style={{ color: '#ebca6d', textTransform: 'uppercase' }} >El carrito está vacío</p></div>: <div></div>}</>
+      <ToastContainer position="top-right" autoClose={1000} newestOnTop />
+      <>
+        {carrito.length === 0 && !detalleFinal ? (
+          <div>
+            <p style={{ color: "#ebca6d", textTransform: "uppercase" }}>
+              El carrito está vacío
+            </p>
+          </div>
+        ) : (
+          <div></div>
+        )}
+      </>
 
-          {carrito.length === 0  ? (<div></div>) : (
-            <div>
-              <div className="card-body">
-                <Table striped bordered hover className="gradient-table">
-                  <thead>
-                    <tr>
-                      <th>Imagen</th>
-                      <th>Nombre</th>
-                      <th>Número</th>
-                      <th>Precio</th>
-                      <th>Cantidad</th>
-                      <th>Total</th>
-                      <th>Acciones</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {carrito.map((producto) => (
-                      <tr key={producto.id_producto}  >
-                        <td>
-                          <img
-                            src={producto.imagen_pequena}
-                            alt={producto.nombre}
-                            style={{ maxHeight: "50px", maxWidth: "50px" }}
-                          />
-                        </td>
-                        <td>{producto.nombre}</td>
-                        <td>{formatearNumeroConPunto(producto.numero)}</td>
-                        <td>{formatearNumeroConPunto(producto.precio)}</td>
-                        <td>
-                          <Button variant="outline-primary" onClick={() => disminuirCantidad(producto.id_producto)}>-</Button>{" "}
-                          {producto.cantidad}{" "}
-                          <Button variant="outline-primary" onClick={() => aumentarCantidad(producto.id_producto)}>+</Button>
-                        </td>
-                        <td>{formatearNumeroConPunto(getTotalPorProducto(producto))}</td>
-                        <td>
-                          <Button variant="danger" onClick={() => eliminarDelCarrito(producto.id_producto)}>
-                            Eliminar del carro
-                          </Button>
-                        </td>
-                      </tr>
-                    ))}
-                  </tbody>
-                </Table>
-              </div>
-              <div><h4 style={{
+      {carrito.length === 0 ? (
+        <div></div>
+      ) : (
+        <div>
+          <div className="card-body">
+            <Table striped bordered hover className="gradient-table">
+              <thead>
+                <tr>
+                  <th className="d-none d-md-table-cell">Imagen</th>
+                  <th className="d-none d-sm-table-cell">Nombre</th>
+                  <th className="d-none d-md-table-cell">Número</th>
+                  <th className="d-none d-md-table-cell">Precio</th>
+                  <th className="d-none d-sm-table-cell">Cantidad</th>
+                  <th className="d-none d-sm-table-cell">Total</th>
+                  <th className="d-none d-sm-table-cell">Acciones</th>
+                </tr>
+              </thead>
+              <tbody>
+                {carrito.map((producto) => (
+                  <tr key={producto.id_producto}>
+                    <td className="d-none d-md-table-cell">
+                      <img
+                        src={producto.imagen_pequena}
+                        alt={producto.nombre}
+                        style={{ maxHeight: "50px", maxWidth: "50px" }}
+                      />
+                    </td>
+                    <td>{producto.nombre}</td>
+                    <td className="d-none d-md-table-cell">{formatearNumeroConPunto(producto.numero)}</td>
+                    <td className="d-none d-md-table-cell">{formatearNumeroConPunto(producto.precio)}</td>
+                    <td>
+                      <Button
+                        variant="outline-primary"
+                        onClick={() => disminuirCantidad(producto.id_producto)}
+                      >
+                        -
+                      </Button>{" "}
+                      {producto.cantidad}{" "}
+                      <Button
+                        variant="outline-primary"
+                        onClick={() => aumentarCantidad(producto.id_producto)}
+                      >
+                        +
+                      </Button>
+                    </td>
+                    <td>
+                      {formatearNumeroConPunto(getTotalPorProducto(producto))}
+                    </td>
+                    <td>
+                      <Button
+                        variant="danger"
+                        onClick={() => eliminarDelCarrito(producto.id_producto)}
+                      >
+                        X
+                      </Button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </Table>
+          </div>
+          <div>
+            <h4
+              style={{
                 backgroundColor: "black",
                 color: "#ebca6d",
                 fontSize: "24px",
-              }}>Total general: ${formatearNumeroConPunto(getTotalGeneral())}</h4></div>
-              <Button variant="primary"
-                className="mr-2 text-uppercase"
-                style={{
-                  backgroundColor: "black",
-                  borderColor: "#ebca6d",
-                  color: "#ebca6d",
-                  fontSize: "12px",
-                }} onClick={procesarCarrito}
-                disabled={isProcessing} // Deshabilitar el botón mientras se está procesando
-              >
-                Procesar carrito
-              </Button>
-            </div>
-
-          )}
-          <div>
-            {/* Utilizamos el componente <pre> para preservar los saltos de línea */}
-            {detalleFinal.length === 0 ? (<div></div>) : (
-              <p className="gradient-table " dangerouslySetInnerHTML={{ __html: detalleFinal }}></p>)
-            }
+              }}
+            >
+              Total general: ${formatearNumeroConPunto(getTotalGeneral())}
+            </h4>
           </div>
-        
+          <Button
+            variant="primary"
+            className="mr-2 text-uppercase"
+            style={{
+              backgroundColor: "black",
+              borderColor: "#ebca6d",
+              color: "#ebca6d",
+              fontSize: "12px",
+            }}
+            onClick={procesarCarrito}
+            disabled={isProcessing} // Deshabilitar el botón mientras se está procesando
+          >
+            Procesar carrito
+          </Button>
+        </div>
+      )}
+      <div>
+        {/* Utilizamos el componente <pre> para preservar los saltos de línea */}
+        {detalleFinal.length === 0 ? (
+          <div></div>
+        ) : (
+          <p
+            className="gradient-table "
+            dangerouslySetInnerHTML={{ __html: detalleFinal }}
+          ></p>
+        )}
+      </div>
     </div>
   );
 };
