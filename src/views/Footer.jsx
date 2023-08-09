9ae99386766822946d0f986e728a86377ca7064c
Leonardo-villagran <leonardovillagran@yahoo.com>
2023-08-08 23:13:37 -0400

    presentación updated

--- src/views/Footer.jsx
+++ src/views/Footer.jsx
@@ -1,8 +1,12 @@
 import Badge from 'react-bootstrap/Badge';
+import { Link, useLocation } from 'react-router-dom'; // Importar el componente Link
 export default function FooterView() {
-
+    const location = useLocation();
     return (
         <div style={{ color: '#ebca6d' }}>
+            {location.pathname === '/' && (
+                <Link to="/presentacion1">Presentación</Link>
+            )}
             <p>Todos los derechos resevados 2023<br /><a href={`mailto:"contacto@comicskites.cl"`}><Badge bg="secondary p-2">contacto@comicskites.cl</Badge></a></p>
         </div>
     )
